28122ab2ac65bd85817550c004f70ba972a613d1
Mostafa Abdelsalam <abdelslam.mostafa@gmail.com>
2020-10-28 21:55:55 +0200

    addin shadow to row and background color

--- src/components/Row.js
+++ src/components/Row.js
@@ -6,7 +6,7 @@ import './Row.css';
 function RowList({data}){
     const {owner} = data;
     return(
-        <div className="dt pv2 pv2-m pv2-ns w-100 bg-light-blue br4">
+        <div className="dt pv2 pv2-m pv2-ns w-100 bg-light-blue br4 shadow-3">
             <Avatar owner={owner} />
             <Words data={data}/>
         </div>
